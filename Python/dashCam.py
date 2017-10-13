1a8c5aaef7d27940520d67d46d27645a016d51eb
WG <wynand@gouws.com.au>
2017-10-13 10:49:01 +0800

    Started dashcam

--- Python/dashCam.py
+++ Python/dashCam.py
@@ -1,9 +1,22 @@
 '''Used to record my dashcam'''
 
+import random
 import picamera
 
+def motion_detected():
+    # Randomly return True (like a fake motion detection routine)
+    return random.randint(0, 10) == 0
+
 camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.start_recording('my_video.h264')
-camera.wait_recording(60)
-camera.stop_recording()
+stream = picamera.PiCameraCircularIO(camera, seconds=20)
+camera.start_recording(stream, format='h264')
+try:
+    while True:
+        camera.wait_recording(1)
+        if motion_detected():
+            # Keep recording for 10 seconds and only then write the
+            # stream to disk
+            camera.wait_recording(10)
+            stream.copy_to('motion.h264')
+finally:
+    camera.stop_recording()
